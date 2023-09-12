5e92a7ea9f85ee73f3cd9893a907674ed0d471ad
Aniket Sen <aniket.singh21@vsit.edu.in>
2023-09-12 10:30:55 +0000

    adding styling

--- src/App.js
+++ src/App.js
@@ -1,73 +1,71 @@
 import { useEffect, useState } from "react";
 import './App.css'
 
+import './style.css'
+import Coincard from "./components/Coincard";
+
 function App() {
-  const [bitval,setBitval]=useState('Loading ...');
-  const [blocks,setBlocks]=useState('Loading ...');
-  const [Transaction,setTransaction]=useState('Loading ...');
-  const [DayChange,setDayChange]=useState('Loading ...');
-  const [hashrate,setHashrate]=useState('Loading ...');
+  const [bitval,setBitval]=useState([]);
+  const [dogecoin,setDogecoin]=useState([]);
+  const [Transaction,setTransaction]=useState([]);
+  const [DayChange,setDayChange]=useState([]);
+  const [hashrate,setHashrate]=useState([]);
 
   useEffect(()=>{
-    async function runthat(){
-      fetch("https://api.blockchair.com/bitcoin/stats",{
+    async function runbit(){
+     const bit=await fetch("https://api.blockchair.com/bitcoin/stats",{
   method:'GET',
   headers:{
     "Content-Type":"application/json"
   }
-}).then((res)=>res.json()).then((data)=>{
-  setBitval(data.data.market_price_usd)
-  setBlocks(data.data.blocks)
-  setTransaction(data.data.transactions)
-  setDayChange(data.data.market_price_usd_change_24h_percentage)
-setHashrate(data.data.hashrate_24h)
-
 })
+
+const data=await bit.json(bit)
+setBitval(data.data)
+console.log(data.data)
+
     }
     
-    runthat()
-    console.log(bitval)
-  })
+    runbit()
+
+    async function rundoge(){
+      const doge=await fetch("https://api.blockchair.com/dogecoin/stats",{
+   method:'GET',
+   headers:{
+     "Content-Type":"application/json"
+   }
+ })
+ 
+ const data=await doge.json(doge)
+ setDogecoin(data.data)
+ console.log(data.data)
+ 
+     }
+     
+     rundoge()
+    
+  },[])
 
   return (
     <>
     <div className="Bitcoin">
       
        
+<center>
 <h1>
-  current bitcoin in usd : ${bitval}
-</h1>
+  Blockchain Data
+  </h1>
+</center>
     </div>
-    <div className="main">
-      <div className="image">
-        <img src="https://loutre.blockchair.io/assets/svg/chains/bitcoin.svg?_nocache=2"></img>
-      </div>
-      <div className="header">
-        <h3>Bitcoin</h3>
-      </div>
-      <div className="price">
-        ${bitval}
-      </div>
-      <div className="icon-link">
-      <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-arrow-up-right-circle-fill" viewBox="0 0 16 16">
-  <path d="M0 8a8 8 0 1 0 16 0A8 8 0 0 0 0 8zm5.904 2.803a.5.5 0 1 1-.707-.707L9.293 6H6.525a.5.5 0 1 1 0-1H10.5a.5.5 0 0 1 .5.5v3.975a.5.5 0 0 1-1 0V6.707l-4.096 4.096z"/>
-</svg>
-      </div>
-      <div className="bottom">
-        <div className="left-content">
-          <p>Blocks</p>
-          <p>Transactions</p>
-          <p>24H Change</p>
-          <p>Hashrate</p>
-        </div>
-        <div className="right-content">
-        <p>{blocks}</p>
-        <p>{Transaction}</p>
-        <p>{DayChange}%</p>
-          <p>{hashrate}</p>
-          </div>
-      </div>
-    </div>
+   <div className="container">
+   
+   <Coincard img={"https://loutre.blockchair.io/assets/svg/chains/bitcoin.svg?_nocache=2"} name={"Bitcoin"} bitval={bitval.market_price_usd} blocks={bitval.blocks}  Transaction={bitval.transactions} DayChange={bitval.market_price_usd_change_24h_percentage} hashrate={bitval.hashrate_24h}/>
+ 
+   <Coincard img={"https://loutre.blockchair.io/assets/svg/chains/dogecoin.svg"} name={"Dogecoin"} bitval={dogecoin.market_price_usd} blocks={dogecoin.blocks}  Transaction={dogecoin.transactions} DayChange={dogecoin.market_price_usd_change_24h_percentage} hashrate={dogecoin.hashrate_24h}/>
+ 
+   
+   </div>
+   
 
     
     </>
---
+++ src/components/Coincard.js
@@ -0,0 +1,47 @@
+
+import '../App.css'
+
+function Coincard(props) {
+
+  return (
+    <>
+<div className='wrapper'>
+<div className="main">
+      <div className="image">
+        <img src={props.img}></img>
+      </div>
+      <div className="header">
+        <h3>{props.name}</h3>
+      </div>
+      <div className="price">
+        ${props.bitval}
+      </div>
+      <div className="icon-link">
+      <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-arrow-up-right-circle-fill" viewBox="0 0 16 16">
+  <path d="M0 8a8 8 0 1 0 16 0A8 8 0 0 0 0 8zm5.904 2.803a.5.5 0 1 1-.707-.707L9.293 6H6.525a.5.5 0 1 1 0-1H10.5a.5.5 0 0 1 .5.5v3.975a.5.5 0 0 1-1 0V6.707l-4.096 4.096z"/>
+</svg>
+      </div>
+      <div className="bottom">
+        <div className="left-content">
+          <p>Blocks</p>
+          <p>Transactions</p>
+          <p>24H Change</p>
+          <p>Hashrate</p>
+        </div>
+        <div className="right-content">
+        <p>{props.blocks}</p>
+        <p>{props.Transaction}</p>
+        <p>{props.DayChange}%</p>
+          <p>{props.hashrate}</p>
+          </div>
+      </div>
+    </div>
+
+</div>
+
+    
+    </>
+  );
+}
+
+export default Coincard;
